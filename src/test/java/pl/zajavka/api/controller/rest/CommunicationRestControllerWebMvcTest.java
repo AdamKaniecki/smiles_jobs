8b53def20fd333ece2261536ff3465febf48f5ac
Adam Kaniecki <adam2113@poczta.onet.pl>
2024-07-31 20:20:31 +0200

    testHiredNotificationBadRequest()

--- src/test/java/pl/zajavka/api/controller/rest/CommunicationRestControllerWebMvcTest.java
+++ src/test/java/pl/zajavka/api/controller/rest/CommunicationRestControllerWebMvcTest.java
@@ -514,4 +514,29 @@ public class CommunicationRestControllerWebMvcTest {
                 .andExpect(content().string("Notification or user not found"));
     }
 
+    @Test
+    void testHiredNotificationBadRequest() throws Exception {
+        String username = "testUser";
+        Authentication authentication = Mockito.mock(Authentication.class);
+        when(authentication.getName()).thenReturn(username);
+
+        when(userService.findByUserName(username)).thenReturn(new User());
+        when(userService.getUserByCv(anyInt())).thenReturn(new User());
+        when(notificationService.findById(anyInt())).thenReturn(NotificationFixtures.sampleNotification1());
+        Mockito.doThrow(new RuntimeException("Some error")).when(notificationService).hiredCandidate(any(), any(), any());
+
+        MeetingInterviewRequest request = new MeetingInterviewRequest();
+        request.setCvId(1);
+        request.setNotificationId(1);
+
+        String jsonRequest = new ObjectMapper().writeValueAsString(request);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/hired")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest)
+                        .principal(authentication))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("An error occurred while hiring candidate"));
+    }
+
 }
